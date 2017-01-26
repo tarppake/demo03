e56c44c6b93c7d53dbec0b66380e4bc2d2932abb
K8230 <K8230@student.jamk.fi>
2017-01-26 15:01:54 +0200

    openkaa

---
+++ HeaterApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heater heater = new Heater();
+            heater.IsOn = true;
+            heater.Temperature = 80;
+            heater.Humidity = 90.0;
+            heater.ThrowWater();
+
+            Console.WriteLine("Temperature is " + heater.Temperature);
+            Console.WriteLine("Humidity is " + heater.Humidity);
+
+
+        }
+
+
+    }
+}
