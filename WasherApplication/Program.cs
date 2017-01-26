d95527b7466653f818a51561ea64a187cb5f1356
K8230 <K8230@student.jamk.fi>
2017-01-26 15:31:06 +0200

    tehtäviä

---
+++ WasherApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasherApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Washer washer = new Washer();
+            washer.IsOn = true;
+            washer.Temperature = 40;
+            washer.Speed = "1200 rpm";
+            washer.On();
+
+            Console.WriteLine("Water temperature is: " + washer.Temperature);
+            Console.WriteLine("Sling speed is: " + washer.Speed);
+
+
+
+        }
+    }
+}
---
+++ WasherApplication/Washer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasherApplication
+{
+    class Washer
+    {
+        public bool IsOn { get; set; }
+        public string Speed { get; set; }
+        public int Temperature { get; set; }
+
+        public void On()
+        {
+            Console.WriteLine("Washer is on");
+        }
+
+
+    }
+}
