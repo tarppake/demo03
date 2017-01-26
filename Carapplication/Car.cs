636101664f6b53b101bfe3f2d568460583001497
K8230 <K8230@student.jamk.fi>
2017-01-26 14:39:29 +0200

    car esim

---
+++ Carapplication/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carapplication
+{
+    class Car
+    {
+        // properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+    
+        // default constructor
+        public Car()
+        {
+
+        }
+    
+        // constructor...
+  
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+
+        // method to give more speed
+        public void Accelerate()
+        {
+            Speed += 10;
+        }
+
+        // method to slow down
+        public void Brake(int value)
+        {
+            Speed -= value; //speed = speed - value;
+        }
+
+        // method to display car data)properties
+        public void PrintData()
+        {
+            Console.WriteLine("Car properties: ");
+            Console.WriteLine("- model: " + Model);
+            Console.WriteLine("- Color: " + Color);
+            Console.WriteLine("- Engine: " + Engine);
+            Console.WriteLine("- Speed: " + Speed);
+            Console.WriteLine("- Dices: " + FuzzyDices);
+            Console.WriteLine("- Doors: " + DoorCount);
+            Console.WriteLine("- Some property: " + SomeProperty);
+        }
+
+        // destructor
+        ~Car()
+        {
+            Console.WriteLine("Car Object destroyed");
+        }
+
+        } //classs
+} // namespce
--- Carapplication/Program.cs
+++ Carapplication/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿using Carapplication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Carapplication
+namespace CarApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // create a new instance from Car class
+            Car car = new Car();
+            car.PrintData();
+            car.Model = "Datsun 100A";
+            car.Color = "Yellow";
+            car.Engine = 0.5;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            car.Accelerate();
+            car.PrintData();
+            car.Brake(7);
+            Car.SomeProperty = 56;
+            car.PrintData();
+
+            // create another car object
+            string model = "Speedster";
+            Car nascar = new Car(model);
+            nascar.PrintData();
+            nascar.Color = "black";
+            Console.WriteLine("Color is " + nascar.Color);
+
+
+            
         }
+
+
     }
 }
